34870fd2247393c9f155d63ee6fb7fc9d9c8f5ca
SGKoishi <i@sgkoi.dev>
2019-06-29 15:19:51 -0700

    Initial commit (WPF)

--- Localizer/Localizer.cs
+++ Localizer/Localizer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
@@ -9,6 +10,12 @@ namespace Localizer
 {
     public class Localizer : Mod
     {
-		
+        public override string Name => Assembly.GetExecutingAssembly().GetName().Name;
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override void Load()
+        {
+
+        }
     }
 }
--- Localizer/Properties/AssemblyInfo.cs
+++ Localizer/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("95d54b28-b67b-4248-b83a-b5e5426c3806")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.2214")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.5")]
