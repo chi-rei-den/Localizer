61ac4ded50efc5989648e6b53073dd8a58c45fe9
SGKoishi <i@sgkoi.dev>
2019-06-30 21:23:00 -0700

    Add ModTranslationManager control

---
+++ LocalizerWPF/ModTranslationManager.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace LocalizerWPF
+{
+    /// <summary>
+    /// Interaction logic for ModTranslationManager.xaml
+    /// </summary>
+    public partial class ModTranslationManager : UserControl
+    {
+        private List<KeyValuePair<string, List<ModTranslation>>> _modTranslations;
+        public List<KeyValuePair<string, List<ModTranslation>>> ModTranslations
+        {
+            get => this._modTranslations;
+            set => this.DataGrid.ItemsSource = this._modTranslations = value;
+        }
+        public ModTranslationManager()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
